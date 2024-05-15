bfba7435c78c582915ccbf84bf2dd6155293f3da
Delsa <delsa.zakeripour@gmail.com>
2024-05-15 16:24:36 +0330

    final travel

--- 05-travel-list/Delsa/src/components/PackageList.js
+++ 05-travel-list/Delsa/src/components/PackageList.js
@@ -1,52 +1,3 @@
-// import { useState } from "react";
-// import Item from "./Item";
-
-// export default function PackingList({
-//   items,
-//   onDeleteItem,
-//   onToggleItem,
-//   onClearList,
-// }) {
-//   const [sortBy, setSortBy] = useState("input");
-
-//   let sortedItems;
-
-//   if (sortBy === "input") sortedItems = items;
-
-//   if (sortBy === "description")
-//     sortedItems = items
-//       .slice()
-//       .sort((a, b) => a.description.localeCompare(b.description));
-
-//   if (sortBy === "packed")
-//     sortedItems = items
-//       .slice()
-//       .sort((a, b) => Number(a.packed) - Number(b.packed));
-
-//   return (
-//     <div className="list">
-//       <ul>
-//         {sortedItems.map((item) => (
-//           <Item
-//             item={item}
-//             key={item.id}
-//             onDeleteItem={onDeleteItem}
-//             onToggleItem={onToggleItem}
-//           />
-//         ))}
-//       </ul>
-
-//       <div className="actions">
-//         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-//           <option value="input"> input order</option>
-//           <option value="description"> description</option>
-//           <option value="packed">packed status</option>
-//         </select>
-//         <button onClick={onClearList}>Clear</button>
-//       </div>
-//     </div>
-//   );
-// }
 import { useState } from "react";
 import Item from "./Item";
 
@@ -56,9 +7,14 @@ export default function PackingList({
   onToggleItem,
   onClearList,
 }) {
-  const [sortBy, setSortBy] = useState("input");
+  const [sortBy, setSortBy] = useState("description");
 
   let sortedItems;
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
   if (sortBy === "input") sortedItems = items;
 
@@ -66,11 +22,6 @@ export default function PackingList({
     sortedItems = items
       .slice()
       .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
   return (
     <div className="list">
