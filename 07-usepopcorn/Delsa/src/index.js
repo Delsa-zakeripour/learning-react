5f5bcafede5f8ce47fac993ce4bd7384122e30a5
Delsa <delsa.zakeripour@gmail.com>
2024-06-16 12:25:03 +0330

    add star rate and usePopcorn project

---
+++ 07-usepopcorn/Delsa/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Flash from "./FlashCards";
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRaiting={5}
+      massage={["awful", "bad", "not bad", "good", "amazing"]}
+    />
+    <StarRating size={22} color={"blue"} className="test" />
+
+    {/* <Flash /> */}
+  </React.StrictMode>
+);
